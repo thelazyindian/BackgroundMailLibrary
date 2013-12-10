67989ae4ff3ac49e484e67e619c6ceade3b675a3
Kristijan Drača <kristijandraca@gmail.com>
2013-12-10 13:14:42 +0100

    first commit

---
+++ BackgroundMailLibrary/gen/com/kristijandraca/backgroundmaillibrary/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.kristijandraca.backgroundmaillibrary;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ BackgroundMailLibrary/src/com/kristijandraca/backgroundmaillibrary/Utils.java
@@ -0,0 +1,13 @@
+package com.kristijandraca.backgroundmaillibrary;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+
+public class Utils {
+
+	public static boolean isNetworkAvailable(Context context) {
+		return ((ConnectivityManager) context
+				.getSystemService(Context.CONNECTIVITY_SERVICE))
+				.getActiveNetworkInfo() != null;
+	}
+}
